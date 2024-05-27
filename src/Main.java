7edd786d37a917342022beb7499f2ae179c77759
smgF2D1 <nikigm0508@abv.bg>
2024-05-27 12:25:55 +0300

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
+
+        Map<String, Set<Students>> yaleStudents = new HashMap<>();
+        Map<String, Set<Students>> harvardStudents = new HashMap<>();
+        Map<String, Set<Students>> princetonStudents = new HashMap<>();
+
         File file = new File("Files/students.txt");
         Scanner in = null;
         try{
@@ -21,7 +29,32 @@ public class Main {
             String major = in.next();
             String university = in.next();
             Students student = new Students(firstName, lastName, toefl, sat, transcript, major, university);
+            if(student.isValid()){
+                switch (university){
+                    case "Yale":
+                        yaleStudents.get(major).add(student);
+                        break;
+                    case "Harvard":
+                        harvardStudents.get(major).add(student);
+                        break;
+                    case "Princeton":
+                        princetonStudents.get(major).add(student);
+                        break;
+
+                }
+            }
         }
         in.close();
+
+        StudentThread stYale = new StudentThread("YaleThread");
+        StudentThread stHarvard = new StudentThread("HarvardThread");
+        StudentThread stPrinceton = new StudentThread("PrincetonThread");
+
+        stYale.start();
+        stHarvard.start();
+        stPrinceton.start();
+
+
+
     }
 }
---
+++ src/StudentThread.java
@@ -0,0 +1,11 @@
+public class StudentThread extends {
+    public StudentThread(String name) {
+        super
+    }
+
+    public StudentThread() {
+    }
+
+    public void start() {
+    }
+}
